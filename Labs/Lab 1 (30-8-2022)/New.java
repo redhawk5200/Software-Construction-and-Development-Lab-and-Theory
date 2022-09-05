b091a6a411a54dd7696b6672a470056b605ff290
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-09-05 22:37:09 +0500

    Add files via upload

---
+++ Labs/Lab 1 (30-8-2022)/New.java
@@ -0,0 +1,8 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println(args[0]+12);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+    }
+}
