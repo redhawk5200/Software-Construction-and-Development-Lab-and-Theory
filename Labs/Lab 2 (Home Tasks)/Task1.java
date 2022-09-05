0b9aeaf78f3efd8667b0f35076098901f9e8bf7d
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-09-05 22:47:23 +0500

    Create Task1.java

---
+++ Labs/Lab 2 (Home Tasks)/Task1.java
@@ -0,0 +1,16 @@
+public class Task1 {
+	public static void main(String[] args) {
+ 
+    	Rectangle rc = new Rectangle(3,4);
+    	rc.generateArea();
+ 
+    	Square sq= new Square(32);
+        sq.generateArea();
+ 
+    	Circle c = new Circle(4);
+        c.generateArea();
+ 
+    	Triangle t = new Triangle(4 ,3);
+        t.generateArea();
+	}
+}
