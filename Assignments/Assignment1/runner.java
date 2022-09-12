086a95ff8d6de306c93d0b0cbdc312d08789ef14
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-09-13 02:37:24 +0500

    Create runner.java

---
+++ Assignments/Assignment1/runner.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class runner {
+	public static void main(String[] args) {
+		Main M = new Main();
+		M.showField();
+	}
+}
